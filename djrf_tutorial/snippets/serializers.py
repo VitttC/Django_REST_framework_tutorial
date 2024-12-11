b30dca074e2e86dee49183e7003e7b8df09d0b0e
VitttC <vittt.caria@gmail.com>
2024-12-12 00:38:10 +0100

    Serialization (w/ ModelSerializer)

--- djrf_tutorial/snippets/serializers.py
+++ djrf_tutorial/snippets/serializers.py
@@ -3,16 +3,13 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
     """
     Defining fields that get serialized/deserialized
     """
-    id =       serializers.IntegerField(read_only=True)
-    title =    serializers.CharField(required=False, allow_blank=True ,max_length=100)
-    code =     serializers.CharField(style={'base_template': 'text_area.html'})
-    linenos =  serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style =    serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
 
     def create(self, validated_data):
         """
