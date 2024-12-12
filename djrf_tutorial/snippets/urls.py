b89a6b5585bfadca3b18e796bb4a0406a4dd0440
VitttC <vittt.caria@gmail.com>
2024-12-12 18:25:00 +0100

    Refactored 'urls.py' to use Router

--- djrf_tutorial/snippets/urls.py
+++ djrf_tutorial/snippets/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
 
 from snippets import views
