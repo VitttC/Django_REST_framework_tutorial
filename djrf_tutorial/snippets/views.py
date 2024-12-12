cd6e5a8902ee8001090cc17b4be71a954f93dc8d
VitttC <vittt.caria@gmail.com>
2024-12-12 04:53:06 +0100

    added user authentication permissions

--- djrf_tutorial/snippets/views.py
+++ djrf_tutorial/snippets/views.py
@@ -1,7 +1,20 @@
-from rest_framework import generics
+from django.contrib.auth.models import User
+
+from rest_framework import generics, permissions
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -10,7 +23,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 """
 Individual snippet view.
@@ -21,3 +37,4 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
