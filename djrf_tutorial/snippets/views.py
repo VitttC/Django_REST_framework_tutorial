2ec3420a43fcf9e8bbdaac3c24a0020659f37b59
VitttC <vittt.caria@gmail.com>
2024-12-12 03:53:58 +0100

    rewritten views to use mixed-in generic classes

--- djrf_tutorial/snippets/views.py
+++ djrf_tutorial/snippets/views.py
@@ -1,42 +1,23 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-        
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-        
 
 """
 Individual snippet view.
 """
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
